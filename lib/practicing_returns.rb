750f2a39d3e55494a065dffd8d76c0409921c3eb
Areeba Zahir <areebazahir3@gmail.com>
2020-08-30 06:52:13 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,13 +1,13 @@
 require "pry"
 
 def hello(array)
-  i = 0	  
+  i = 0
   collection = []
   while i < array.length
-   collection << yield(array[i])
-    i += 1	   
+    collection << yield(array[i])
+    i += 1
   end
   collection
-end	
+end
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(list) {|i| "Hello #{i} what's up?"}
